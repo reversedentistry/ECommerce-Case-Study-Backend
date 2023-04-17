98dc169f330a1ad1a335ff04732268652826617e
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-17 15:06:49 -0700

    Added repository annotation

--- src/main/java/com/revature/ecommerce/repository/UserRepository.java
+++ src/main/java/com/revature/ecommerce/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.revature.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.revature.ecommerce.model.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    User findByUsername(String username);
 }
