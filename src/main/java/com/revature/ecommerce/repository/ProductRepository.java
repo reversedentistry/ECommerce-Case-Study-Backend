a0c44fb6ba3fa793ecee1f6ce18844a4cec82332
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-17 09:40:31 -0700

    Added in JPA inheritance

--- src/main/java/com/revature/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/revature/ecommerce/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.revature.ecommerce.repository;
 
-public interface ProductRepository {
+import com.revature.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
--- src/main/java/com/revature/ecommerce/repository/UserRepository.java
+++ src/main/java/com/revature/ecommerce/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.revature.ecommerce.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.revature.ecommerce.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
