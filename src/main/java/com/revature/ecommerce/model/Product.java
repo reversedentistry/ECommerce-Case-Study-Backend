8528ca3df0ded94ea356a1ee1e3898ed6339f6f6
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-17 09:41:05 -0700

    Created fields and added Lombok annotations

--- src/main/java/com/revature/ecommerce/model/Product.java
+++ src/main/java/com/revature/ecommerce/model/Product.java
@@ -1,4 +1,30 @@
 package com.revature.ecommerce.model;
 
+import lombok.*;
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Product {
+    @Id
+    private int productId;
+    private double price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && Double.compare(product.price, price) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, price);
+    }
 }
--- src/main/java/com/revature/ecommerce/model/User.java
+++ src/main/java/com/revature/ecommerce/model/User.java
@@ -1,4 +1,31 @@
 package com.revature.ecommerce.model;
 
+import lombok.*;
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "ecommerceUser")
 public class User {
+    @Id
+    private int userId;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, password);
+    }
 }
