0d23f19849a920b04b8e551131bd5c99d388835d
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-17 15:08:04 -0700

    Created methods for service classes

--- src/main/java/com/revature/ecommerce/service/ProductService.java
+++ src/main/java/com/revature/ecommerce/service/ProductService.java
@@ -1,4 +1,21 @@
 package com.revature.ecommerce.service;
 
+import com.revature.ecommerce.model.Product;
+import com.revature.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
 public class ProductService {
+    @Autowired
+    ProductRepository productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product getProduct(int productId) throws NoSuchElementException{
+        return productRepo.findById(productId).get();
+    }
 }
--- src/main/java/com/revature/ecommerce/service/UserService.java
+++ src/main/java/com/revature/ecommerce/service/UserService.java
@@ -1,4 +1,26 @@
 package com.revature.ecommerce.service;
 
+import com.revature.ecommerce.model.User;
+import com.revature.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepository userRepo;
+
+    public User createNewUser(User user) {
+        return userRepo.save(user);
+    }
+
+    public boolean userLogin(String username, String password) {
+        User loginAttempt = userRepo.findByUsername(username);
+        if (loginAttempt != null) {
+            String userPassword = loginAttempt.getPassword();
+            return password.equals(userPassword);
+        } else {
+            return false;
+        }
+    }
 }
