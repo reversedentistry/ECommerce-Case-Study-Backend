58bbda8d729692d093fe6a3d600aed4d1469b7b9
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-17 15:08:51 -0700

    Created methods to retrieve product info

--- src/main/java/com/revature/ecommerce/controller/ProductController.java
+++ src/main/java/com/revature/ecommerce/controller/ProductController.java
@@ -1,4 +1,32 @@
 package com.revature.ecommerce.controller;
 
+import com.revature.ecommerce.model.Product;
+import com.revature.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProduct(int productId) {
+        try {
+            Product product = productService.getProduct(productId);
+            return ResponseEntity.status(200).body(product);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
