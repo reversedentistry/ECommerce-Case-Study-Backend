82138ea468564268dceee5b90af96b112fd49df4
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-18 14:16:45 -0700

    Created file for populated data upon database initialization

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (product_id, product_name, price) VALUES (1, 'item1', 15.00);
+INSERT INTO product (product_id, product_name, price) VALUES (2, 'item2', 20.00);
+INSERT INTO product (product_id, product_name, price) VALUES (3, 'item3', 10.00);
+INSERT INTO product (product_id, product_name, price) VALUES (4, 'item4', 18.00);
+INSERT INTO product (product_id, product_name, price) VALUES (5, 'item5', 7.00);
