47348840b7f000bfdf05c27d8d9e78f58cd8c62b
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-18 14:17:54 -0700

    Added requestbody annotation to controllers to fix bug

--- src/main/java/com/revature/ecommerce/controller/UserController.java
+++ src/main/java/com/revature/ecommerce/controller/UserController.java
@@ -5,6 +5,7 @@ import com.revature.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,14 +14,14 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(User user) {
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
         User newUser = userService.createNewUser(user);
         return ResponseEntity.status(200).body(newUser);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(String username, String password) {
-        if (userService.userLogin(username, password)) {
+    public ResponseEntity<String> loginUser(@RequestBody User user) {
+        if (userService.userLogin(user)) {
             return ResponseEntity.status(200).body("Successful login!");
         } else {
             return ResponseEntity.status(400).body("There was an error " +
