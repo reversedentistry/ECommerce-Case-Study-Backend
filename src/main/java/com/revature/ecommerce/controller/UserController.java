6611335d690658cd42a899e85ae148286d0910fd
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-19 15:59:29 -0700

    Added cross origin annotation

--- src/main/java/com/revature/ecommerce/controller/UserController.java
+++ src/main/java/com/revature/ecommerce/controller/UserController.java
@@ -4,11 +4,13 @@ import com.revature.ecommerce.model.User;
 import com.revature.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserController {
     @Autowired
     UserService userService;
