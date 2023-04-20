46c8dbdf52f77fd0c1deeac609f2fb7ea24a5612
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-20 08:07:54 -0700

    Deleted order layers

--- src/main/java/com/revature/ecommerce/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.ecommerce.repository;
-
-import com.revature.ecommerce.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository  extends JpaRepository<Order, Long> {
-}
